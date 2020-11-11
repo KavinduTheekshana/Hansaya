fa9f33508974a8b718ef60080c7608e6c5ca2c22
Kavindu Theekshana <kavindutheekshana@Kavindus-MacBook-Pro.local>
2020-11-11 11:29:09 +0530

    Auth Bug Fix

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
